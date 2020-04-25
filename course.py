2ba012b1093686ae9104c111204cb697b60702de
varunrajrana <varunrajrana98@gmail.com>
2020-04-24 21:09:24 -0600

    hwoop1

--- course.py
+++ course.py
@@ -1,11 +1,29 @@
+coordinate1 = (3,2)
+coordinate2 = (8,10)
 
-x='The quick brown fox jumps over the lazy dog'
-#x=int(input('Num1:'))
-#y=int(input('Num2:'))
-
-def myfunc(x,alphabet=string.ascii_lowercase):
-    pass
-
+class Line:
     
+    def __init__(self,coor1,coor2):
+        self.coor1=coor1
+        self.coor2=coor2
+    
+    def distance(self):
+        dx=abs(self.coor2[0]-self.coor1[0])
+        print(dx)
+        dy=abs(self.coor2[1]-self.coor1[1])
+        print(dy)
+        distance=(dx**2 +dy**2)**0.5
+        print(distance)
 
-print(myfunc(x))
+    def slope(self):
+        dx=abs(self.coor2[0]-self.coor1[0])
+        print(dx)
+        dy=abs(self.coor2[1]-self.coor1[1])
+        print(dy)
+        slope=dy/dx
+        print(slope)
+        pass
+    
+li = Line(coordinate1,coordinate2)
+
+li.slope()
